54ffdd4c1489f4d2f9285b1f0cbd227492eba1a0
fmndantas <fernandodantas1996@gmail.com>
2019-07-17 17:58:34 -0300

    GameBoard -> Game, async start and several changes

--- tetris/tetris.py
+++ tetris/tetris.py
@@ -1,8 +1,12 @@
+import asyncio
+from collections import namedtuple
+from concurrent.futures import FIRST_COMPLETED
+
+import keyboard
 import numpy as np
 from numpy.random import choice
-from collections import namedtuple
 
-Point = namedtuple('Point', 'x y')
+Point = namedtuple('Point', ('x', 'y'))
 
 
 class Block(object):
@@ -16,20 +20,12 @@ class Block(object):
             return u"\u25A0"
         return u"\u25A1"
 
-    @property
-    def gb_position(self):
-        return self.gameboard_position
-
-    @gb_position.setter
-    def gb_position(self, gb_pos):
-        self.gameboard_position = gb_pos
-
 
 class Shape(object):
-    def __init__(self, height=2, width=2, description=None, color='w', shape_id=None):
-        self.height = height
+    def __init__(self, width=2, height=2, description=None, color='w', shape_id=None):
         self.width = width
-        self.grid = np.zeros((self.height, self.width), object)
+        self.height = height
+        self.grid = np.zeros((self.width, self.height), object)
         self.pivot = None
         self.color = color
         self.set_shape(description)
@@ -51,6 +47,41 @@ class Shape(object):
     def rot90_fall_blck(self, k=1):
         self.grid = np.rot90(self.grid, k=k)
 
+    def can_block_fall(self, gameboard):
+        for lin in range(self.left, self.right):
+            if isinstance(gameboard[lin, self.bottom - 1], Block):
+                return False
+            continue
+        return True
+
+    @property
+    def bottom(self):
+        return min(self.grid[lin, col].gameboard_position[1]
+                   for lin in range(self.width)
+                   for col in range(self.height)
+                   if isinstance(self.grid[lin, col], Block))
+
+    @property
+    def top(self):
+        return max(self.grid[lin, col].gameboard_position[1]
+                   for lin in range(self.width)
+                   for col in range(self.height)
+                   if isinstance(self.grid[lin, col], Block))
+
+    @property
+    def left(self):
+        return min(self.grid[lin, col].gameboard_position[0]
+                   for lin in range(self.width)
+                   for col in range(self.height)
+                   if isinstance(self.grid[lin, col], Block))
+
+    @property
+    def right(self):
+        return max(self.grid[lin, col].gameboard_position[0]
+                   for lin in range(self.width)
+                   for col in range(self.height)
+                   if isinstance(self.grid[lin, col], Block))
+
     def __repr__(self):
         return str(self.grid).replace('[', ' ').replace(']', ' ').replace('0', ' ')
 
@@ -60,29 +91,28 @@ class Shape(object):
 
 class ShapeGenerator:
     def __init__(self):
-        shape_1 = Shape(1, 4, shape_id=1)                                     # rect - revised
-        shape_1.set_pivot((0, 2))
+        shape_1 = Shape(4, 1, shape_id=1)  # rect
+        shape_1.set_pivot((2, 0))
 
-        shape_2 = Shape(3, 2, ((0, 0), (1, 0), (2, 0), (2, 1)), shape_id=2)   # ell - revised
-        shape_2.set_pivot((1, 0))
+        shape_2 = Shape(2, 3, ((0, 0), (0, 1), (0, 2), (1, 0)), shape_id=2)  # ell
+        shape_2.set_pivot((0, 1))
 
-        shape_3 = Shape(3, 2, ((0, 1), (1, 1), (2, 1), (2, 0)), shape_id=3)   # mirrored ell - revised
+        shape_3 = Shape(2, 3, ((0, 0), (1, 0), (1, 1), (1, 2)), shape_id=3)  # mirrored ell
         shape_3.set_pivot((1, 1))
 
-        shape_4 = Shape(2, 2, shape_id=4)                                     # square - revised
-        # no pivot
+        shape_4 = Shape(shape_id=4)  # square
+        shape_4.set_pivot((1, 1))
 
-        shape_5 = Shape(2, 3, ((1, 0), (1, 1), (0, 1), (0, 2)), shape_id=5)   # unfinished 's'
-        shape_5.set_pivot((1, 1))
+        shape_5 = Shape(3, 2, ((0, 0), (1, 0), (1, 1), (2, 1)), shape_id=5)  # unfinished 's'
+        shape_5.set_pivot((1, 0))
 
-        shape_6 = Shape(2, 3, ((0, 0), (0, 1), (1, 1), (1, 2)), shape_id=6)   # unfinished 'z'
-        shape_6.set_pivot((1, 1))
+        shape_6 = Shape(3, 2, ((0, 1), (1, 1), (1, 0), (2, 0)), shape_id=6)  # unfinished 'z'
+        shape_6.set_pivot((1, 0))
 
-        shape_7 = Shape(2, 3, ((1, 0), (1, 1), (0, 1), (1, 2)), shape_id=7)  # symmetrical ell
-        shape_7.set_pivot((1, 1))
+        shape_7 = Shape(3, 2, ((0, 0), (1, 0), (2, 0), (1, 1)), shape_id=7)  # symmetrical ell
+        shape_7.set_pivot((1, 0))
 
         self.shapes = [shape_1, shape_2, shape_3, shape_4, shape_5, shape_6, shape_7]
-        for shape in self.shapes: shape.rot90_fall_blck(2)
 
     def __call__(self, *args, **kwargs):
         return self.shape_generator()
@@ -92,14 +122,20 @@ class ShapeGenerator:
             yield choice(self.shapes)
 
 
-class GameBoard(object):
+_LEVELS = {1: 5}
+
+
+class Game(object):
     def __init__(self, width=10, height=10):
         self.width = width
         self.height = height
-        self.game_board = np.zeros((height, width), object)
+        self.gameboard = np.zeros((width, height), object)
         self.falling_shape = None
         self.sg = ShapeGenerator()()
         self.pivot_initial_pos = {0: (0, 10)}
+        self.is_game_over = False
+        self.level = 1
+        self.keys = {'right': 'd', 'left': 'a', 'down': 'd', 'rotate': 'r'}
 
     def generate_next_shape(self):
         self.falling_shape = self.sg.__next__()
@@ -107,12 +143,61 @@ class GameBoard(object):
     def put_shape_in_gameboard(self, intl_pnt: Point) -> None:
         pivot = self.falling_shape.pivot
         grid = self.falling_shape.grid
+        for lin in range(self.falling_shape.width):
+            for col in range(self.falling_shape.height):
+                if isinstance(grid[lin][col], Block):
+                    distx = lin - pivot.x
+                    disty = col - pivot.y
+                    gb = Point(intl_pnt.x + distx, intl_pnt.y + disty)
+                    self.gameboard[gb.x, gb.y] = grid[lin, col]
+                    self.gameboard[gb.x, gb.y].gameboard_position = gb.x, gb.y  # changing Blocks.gameboard_position
 
-    def can_block_fall(self):
+    def move_falling_shape_right(self, k=1):
+        print('falling shape to right')
+
+    def move_falling_shape_left(self, k=1):
+        print('falling shape to left')
+
+    def move_falling_shape_down(self, k=1):
+        print('move falling shape down')
+
+    def rotate_falling_shape_clockwise(self, k=1):
+        print('rotate falling shape clockwise')
+
+    @asyncio.coroutine
+    def right(self, k=1):
+        while 1:
+            if keyboard.is_pressed(self.keys['right']):
+                self.move_falling_shape_right(k=k)
+            yield from asyncio.sleep(0)
+
+    @asyncio.coroutine
+    def left(self, k=1):
+        while 1:
+            if keyboard.is_pressed(self.keys['left']):
+                self.move_falling_shape_left(k=k)
+            yield from asyncio.sleep(0)
+
+    @asyncio.coroutine
+    def timer(self):
+        keyboard.unhook_all()
+        yield from asyncio.sleep(_LEVELS[self.level])
+
+    @asyncio.coroutine
+    def __inner_loop(self):
+        futures = [self.right(), self.left(), self.timer()]
+        done, pendent = yield from asyncio.wait(futures, return_when=FIRST_COMPLETED)
+        for future in pendent:
+            future.cancel()
+
+    def game_over(self):
         pass
 
-    def fall_block(self):
-        pass
-
-    def loop(self):
-        pass
+    def start(self):
+        while not self.is_game_over:
+            self.generate_next_shape()
+            self.put_shape_in_gameboard(Point(5, 5))
+            ioloop = asyncio.new_event_loop()
+            ioloop.run_until_complete(self.__inner_loop())
+            ioloop.close()
+        self.game_over()
